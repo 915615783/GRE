3d7384460fc2f92007aafce0109c32a73cdb2c42
915615783 <915615783@qq.com>
2020-04-17 21:53:26 +0800

    create some file and dir

---
+++ models/gre.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+
+class Gre():
+    '''
+    Input the batch data. Output the loss, metric and some output tensor.
+    '''
+    def __init__(self):
+        pass
